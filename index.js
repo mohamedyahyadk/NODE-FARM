ca9a1082df2a51326f21eabb4431ecd0c4d443ca
mohamedyahyadk <154635643+mohamedyahyadk@users.noreply.github.com>
2025-09-28 13:35:32 -1100

    feat(server) : template-card data
    -read card template file
    -import  products data/json  synchronously
    -parse json file
    -add logic:map func to loop over card
    -add repalceTemplate func to replace each tamplate card placeholders  by data

--- index.js
+++ index.js
@@ -8,7 +8,29 @@ const templateOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   "utf-8"
 );
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
 
+// logic
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
+  return output;
+};
+
+// import data
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
 // creating the server
 const server = http.createServer((req, res) => {
   const { pathname } = url.parse(req.url, true);
@@ -16,7 +38,11 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {
       "content-type": "text/html",
     });
-    const output = templateOverview;
+    const cardHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+
+    const output = templateOverview.replace("{%PRODUCT-CARDS%}", cardHtml);
     res.end(output);
   } else {
     res.writeHead(404, {
